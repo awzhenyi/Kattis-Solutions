a3b966d25726d7bc643ba6e7264847d7283cad2b
awzhenyi <75432394+awzhenyi@users.noreply.github.com>
2020-12-03 23:09:52 +0800

    Add files via upload

---
+++ Codes/aaaaaah.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	string a,b;
+	cin >> a >> b;
+	if(a.length()<b.length())
+		cout << "no";
+	else
+		cout << "go";
+	return 0;
+} 
